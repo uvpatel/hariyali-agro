3e59489b59344a56f6e92dd8badf701f7719f630
Urvil Patel <uvpatel7271@gmail.com>
2026-08-11 19:58:21 +0530

    Hero section database connection dashboard implementation

---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
+
+export default function Hero() {
+  return (
+    <div className="relative flex min-h-screen w-full flex-col items-start justify-start overflow-hidden">
+      <BackgroundRippleEffect />
+      <div className="mt-60 w-full">
+        <h2 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-neutral-800 md:text-4xl lg:text-7xl dark:text-neutral-100">
+         Hariyali Fertilizer and Agro Center
+        </h2>
+        <p className="relative z-10 mx-auto mt-4 max-w-xl text-center text-neutral-800 dark:text-neutral-500">
+          We are a leading provider of high-quality fertilizers and agricultural products, dedicated to supporting farmers and promoting sustainable farming practices. Our mission is to enhance crop yields and improve the livelihoods of farmers through innovative solutions and exceptional customer service.
+        </p>
+      </div>
+    </div>
+  );
+}
